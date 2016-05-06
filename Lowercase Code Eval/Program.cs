58ad69231704afdca9b454e707b7cf633b1ceb46
Your Name <your@example.com>
2016-05-06 14:02:14 -0400

    LowerCase_CodeEval_Initial

---
+++ Lowercase Code Eval/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lowercase_Code_Eval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "Hello CodeEval";
+            Console.WriteLine(line.ToLower());
+        }
+    }
+}
